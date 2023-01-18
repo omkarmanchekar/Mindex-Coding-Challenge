929eed1b3931130aecfc52b379d56ff672677961
omkarmanchekar <omkar.manchekar@somaiya.edu>
2023-01-18 18:59:03 -0500

    better approach of compensation

--- src/main/java/com/mindex/challenge/dao/CompensationRepository.java
+++ src/main/java/com/mindex/challenge/dao/CompensationRepository.java
@@ -3,13 +3,10 @@ package com.mindex.challenge.dao;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 import com.mindex.challenge.data.Compensation;
 
 @Repository
 public interface CompensationRepository extends MongoRepository<Compensation, String>{
-    List<Compensation> findBy(String id);
-    // Compensation findCompensationByEmployeeId(String id);
     Compensation findByEmployeeEmployeeId(String id);
 }
